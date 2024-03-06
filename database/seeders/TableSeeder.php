78b40327cba2bbe29c7acbbee0f6af4db428f21f
S4ntifdz <safernandez@frro.utn.edu.ar>
2024-03-06 01:43:42 -0300

    Se crearon las rutas, se corrieron los seeds, se comienza con los collection resources

--- database/seeders/TableSeeder.php
+++ database/seeders/TableSeeder.php
@@ -4,7 +4,10 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
+use App\Models\Menu;
+use App\Models\Item;
+use App\Models\Order;
+use App\Models\Table;
 class TableSeeder extends Seeder
 {
     /**
@@ -12,6 +15,12 @@ class TableSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+
+        $tables = Table::factory(50)->create();
+
+        $tables->each(function ($table) {
+            $table->orders()->save(Order::factory()->make());
+        });
+
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\ItemController;
+use App\Http\Controllers\MenuController;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\TableController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +20,20 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::group(['prefix'=>'v1'],function(){
+    Route::apiResource('Orders', OrderController::class);
+    Route::post('Orders/{table_id}/items', [OrderController::class, 'addItems']);
+    Route::get('Orders/{id}/tables', [OrderController::class, 'tables']);
+
+    Route::apiResource('Items', ItemController::class);
+    Route::get('Items/{id}', [ItemController::class]);
+
+    Route::apiResource('Menus', MenuController::class);
+    Route::get('Menus/{id}/items', [MenuController::class, 'items']);
+
+    Route::apiResource('Tables', TableController::class);
+    Route::get('Tables/{id}/orders', [TableController::class, 'orders']);
+
 });
